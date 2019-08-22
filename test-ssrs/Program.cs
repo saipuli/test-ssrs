8246992893c933976642c425e3892ab9a4445b98
Sai Puli <sai.puli@getbeyond.com>
2019-08-22 06:37:40 -0700

    testing a middleman mechanism for SSRS.

--- test-ssrs/Program.cs
+++ test-ssrs/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net;
+using System.Text;
 
 namespace test_ssrs
 {
@@ -6,7 +9,36 @@ namespace test_ssrs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var userName = Environment.GetEnvironmentVariable("Creds_UserName");
+            var password = Environment.GetEnvironmentVariable("Creds_Password");
+            CredentialCache credentialCache = new CredentialCache();
+            credentialCache.Add(
+                new Uri("https://beyondssrsdev.eastus.cloudapp.azure.com")
+                , "NTLM"
+                , new NetworkCredential(
+                    userName
+                    , password
+                    , "beyondssrsdev.eastus.cloudapp.azure.com"));
+            using (var httpClientHandler = new HttpClientHandler() { Credentials = credentialCache })
+            {
+                // Use ServerCertificateCustomValidationCallback with extreme caution.
+                httpClientHandler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => { return true; };
+
+                using (var client = new HttpClient(httpClientHandler))
+                {
+                    // Set up the call.
+                    var requestUri = new Uri("https://beyondssrsdev.eastus.cloudapp.azure.com/ReportServer/Pages/ReportViewer.aspx?%2fCompensation%2fPortfolioHealthbyDD&rs:Command=Render");
+                    var payload = "{\"DDEmail\": \"ken.tow@getbeyond.com\"}";
+                    var content = new StringContent(payload, Encoding.UTF8, "application/json");
+                    // Make the call.
+                    var response = client.PostAsync(requestUri, content).Result;
+                    // Manipulate the results.
+                    var result = response.Content.ReadAsStringAsync().Result;
+
+                    Console.WriteLine("Server Response: " + result);
+                    Console.ReadLine();
+                }
+            }
         }
     }
 }
